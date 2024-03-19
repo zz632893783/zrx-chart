e227c66ac90f58f593766dc00d636f9d230337b9
Zhang Zheng <632893783@qq.com>
2024-03-19 14:00:36 +0800

    现阶段文档全部完成

---
+++ document/ringChart5/支持方法.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="table">
+		<div class="table-header">
+			<div class="table-row">
+				<div class="table-cell">方法名</div>
+				<div class="table-cell">说明</div>
+				<div class="table-cell">例子</div>
+			</div>
+		</div>
+		<div class="table-body">
+			<div class="table-row">
+				<div class="table-cell">renderChart</div>
+				<div class="table-cell">渲染图表</div>
+				<div class="table-cell">chartRef?.value?.renderChart()</div>
+			</div>
+			<div class="table-row">
+				<div class="table-cell">clearChart</div>
+				<div class="table-cell">清除图表</div>
+				<div class="table-cell">chartRef?.value?.clearChart()</div>
+			</div>
+		</div>
+	</div>
+</template>
+<style lang="scss" scoped>
+.table-row {
+	grid-template-columns: minmax(0, 2fr) minmax(0, 2fr) minmax(0, 3fr);
+}
+</style>
